b901421689bf9cee3e7be3e98a9f83d81f01bf10
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-04-24 19:26:52 -0400

    added move case for sorry card

--- src/sorryGame/HardComputerPlayer.java
+++ src/sorryGame/HardComputerPlayer.java
@@ -14,14 +14,46 @@ public class HardComputerPlayer extends ComputerPlayer {
 		for (Map.Entry<String, Pawn[]> pawns : pawnMap.entrySet()){
 			if(!pawns.getKey().equalsIgnoreCase(getColor())){
 				for(Pawn pawn : pawns.getValue()){
-					// if a move results in a bump
-					if(board.willBumpPiece(card, pawn.getLocation())){
-						if(board.validateMove(card, pawn.getID(), 0, 0, 0, 0)){
-							//make the first move that results in a bump
-							board.makeMove(card, pawn.getID(), 0, 0);
-							return;
-						}
+					switch(card){
+						case 0:
+							playerPawns = pawnMap.get(color);
+							destination = pawn.startIndex;
+							for(Pawn p : playerPawns){
+								if(p.getLocation() == p.startSquare.getLocation()){
+									if((destination - pawn.getLocation() +60)%60 < (destination - p.getLocation() + 60)%60){
+										board.makeMove(card, p.getID(), p.getLocation(), destination);
+									}
+								}
+							}
+							break;
+						case 11:
+							break;
+						default:
+							// if a move does not result in a bump ]
+							System.out.println(pawn.getLocation());
+							if (pawn.getLocation() == -1 && (card == 1 || card == 2) && !board.getGameArray()[pawn.startIndex].isOccupied()) {
+								board.makeMove(card, pawn.getID(), pawn.getLocation(), pawn.startIndex);
+								return;
+							}
+							else if (pawn.getLocation() >= 0 && pawn.getLocation() <= 59) {
+								if (board.willBumpPiece(card, pawn.getLocation())) {
+									if (board.validateMove(card, pawn.getID(), pawn.getLocation(),
+														   ((pawn.getLocation() + card) % 60), 0, 0)) {
+										//make rthe first move that results in a bump
+										board.makeMove(card, pawn.getID(), pawn.getLocation(),
+													   ((pawn.getLocation() + card) % 60));
+										return;
+									}
+								}
+								else {
+									System.out.println("should move " + pawn.getID() + " from " + pawn.getLocation() +  " to " + ((pawn.getLocation() + card) % 60));
+									board.makeMove(card, pawn.getID(), pawn.getLocation(), ((pawn.getLocation() + card) % 60));
+									return;
+								}
+							}						
+							break;					
 					}
+					
 				}
 			}
 		}
